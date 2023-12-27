038afb4a466a6895317ec69589096e644a2c27e2
anteater <greatone0520@gmail.com>
2023-12-27 10:55:41 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<div id="app">
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  #app {
+    left: 0px;
+    top: 0px;
+    width: 1300px;
+    height: 780px;
+    position: absolute;
+	background-color: aqua;
+  }
+
+  main {
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<canvas id="canvas"></canvas>
+
+<style>
+  #canvas {
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+</style>
