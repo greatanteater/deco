de522e7556468e6ea2ec1d1490d10bf7775419f7
andrebono <greatone0520@gmail.com>
2024-01-05 10:11:14 +0900

    .

--- src/activity/scene/DecoData.ts
+++ src/activity/scene/DecoData.ts
@@ -6,6 +6,10 @@ export interface Position {
   y: number;
 }
 
+export interface HairCoordinate {
+  coordinates: Position[];
+}
+
 export interface Face {
   displacement: Pixi.Sprite;
   sprite: Pixi.Sprite;
@@ -13,6 +17,7 @@ export interface Face {
   hairSprite: Pixi.Sprite;
   hairGraphic: SmoothGraphics;
   charNumber: number;
+  hairCoordinate: Position[];
 }
 export interface FaceContainer {
   container: Pixi.Container;
@@ -42,3 +47,96 @@ export interface mouse {
   path: string;
   position: Position;
 }
+
+export const hairCoordinates: HairCoordinate[] = [{
+  coordinates: [
+    { x: 538, y: 169 },
+      { x: 546, y: 163 },
+      { x: 552, y: 157 },
+      { x: 560, y: 152 },
+      { x: 568, y: 149 },
+      { x: 573, y: 586 },
+      { x: 586, y: 595 },
+      { x: 607, y: 129 },
+      { x: 620, y: 124 },
+      { x: 635, y: 120 },
+      { x: 646, y: 118 },
+      { x: 664, y: 115 },
+      { x: 674, y: 115 },
+      { x: 688, y: 117 },
+      { x: 702, y: 119 },
+      { x: 712, y: 123 },
+      { x: 724, y: 128 },
+      { x: 736, y: 133 },
+      { x: 754, y: 145 },
+      { x: 761, y: 155 },
+      { x: 774, y: 168 },
+      { x: 782, y: 177 },
+      { x: 790, y: 191 },
+      { x: 794, y: 198 },
+      { x: 798, y: 204 },
+      { x: 808, y: 213 },
+      { x: 823, y: 222 },
+      { x: 834, y: 229 },
+      { x: 845, y: 233 },
+      { x: 868, y: 233 },
+      { x: 894, y: 236 },
+      { x: 913, y: 237 },
+      { x: 929, y: 238 },
+      { x: 947, y: 238 },
+      { x: 962, y: 238 },
+      { x: 971, y: 238 },
+      { x: 974, y: 237 },
+      { x: 970, y: 226 },
+      { x: 971, y: 210 },
+      { x: 968, y: 202 },
+      { x: 962, y: 185 },
+      { x: 959, y: 170 },
+      { x: 948, y: 145 },
+      { x: 932, y: 123 },
+      { x: 910, y: 108 },
+      { x: 891, y: 97 },
+      { x: 869, y: 87 },
+      { x: 856, y: 81 },
+      { x: 836, y: 72 },
+      { x: 814, y: 68 },
+      { x: 793, y: 65 },
+      { x: 764, y: 61 },
+      { x: 720, y: 58 },
+      { x: 688, y: 59 },
+      { x: 658, y: 56 },
+      { x: 632, y: 54 },
+      { x: 589, y: 55 },
+      { x: 566, y: 57 },
+      { x: 514, y: 63 },
+      { x: 492, y: 65 },
+      { x: 458, y: 71 },
+      { x: 434, y: 79 },
+      { x: 423, y: 83 },
+      { x: 414, y: 89 },
+      { x: 402, y: 95 },
+      { x: 396, y: 105 },
+      { x: 387, y: 115 },
+      { x: 382, y: 120 },
+      { x: 379, y: 133 },
+      { x: 376, y: 141 },
+      { x: 372, y: 150 },
+      { x: 364, y: 160 },
+      { x: 358, y: 198 },
+      { x: 350, y: 184 },
+      { x: 338, y: 198 },
+      { x: 327, y: 214 },
+      { x: 324, y: 223 },
+      { x: 324, y: 229 },
+      { x: 338, y: 233 },
+      { x: 359, y: 237 },
+      { x: 384, y: 240 },
+      { x: 418, y: 238 },
+      { x: 441, y: 233 },
+      { x: 472, y: 220 },
+      { x: 491, y: 210 },
+      { x: 511, y: 193 },
+      { x: 524, y: 179 },
+      { x: 538, y: 169 },
+  ]
+}];
--- src/activity/scene/DecoDrawing.ts
+++ src/activity/scene/DecoDrawing.ts
@@ -1,13 +1,13 @@
-import * as Pixi from "pixi.js";
-import Setting from "../base/Setting";
-import { wait } from "../util/Util";
-import { get } from "svelte/store";
-import { currentView, characterNumber } from "../store/store";
-import { gsap } from "gsap";
-import * as Data from "./DecoData";
-import DecoScene from "./DecoScene";
-import { SmoothGraphics, LINE_SCALE_MODE } from "@pixi/graphics-smooth";
-import { OutlineFilter } from "@pixi/filter-outline";
+import * as Pixi from 'pixi.js';
+import Setting from '../base/Setting';
+import { wait } from '../util/Util';
+import { get } from 'svelte/store';
+import { currentView, characterNumber } from '../store/store';
+import { gsap } from 'gsap';
+import * as Data from './DecoData';
+import DecoScene from './DecoScene';
+import { SmoothGraphics, LINE_SCALE_MODE } from '@pixi/graphics-smooth';
+import { OutlineFilter } from '@pixi/filter-outline';
 
 export default class DecoDrawing extends Pixi.Container {
   private down = false;
@@ -23,7 +23,7 @@ export default class DecoDrawing extends Pixi.Container {
   private charNumber = 0;
   private faceHeight = 0;
   private faceContainer: Data.FaceContainer[] = [];
-  private drawTarget: string = "";
+  private drawTarget: string = '';
 
   constructor(scene: DecoScene) {
     super();
@@ -31,11 +31,11 @@ export default class DecoDrawing extends Pixi.Container {
   }
 
   private async initialize() {
-    this.eventMode = "static";
+    this.eventMode = 'static';
     this.faceHeight = 400;
-    this.drawTarget = "face";
+    this.drawTarget = 'face';
     await this.setFaces();
-    this.setFacesPosition("default");
+    this.setFacesPosition('default');
     this.setButton();
     this.loadStore();
     this.setUpEventListeners();
@@ -43,19 +43,19 @@ export default class DecoDrawing extends Pixi.Container {
   }
 
   private setUpEventListeners() {
-    this.on("pointerdown", this.onPointerDown, this);
-    this.on("pointermove", this.onPointerMove, this);
-    this.on("pointerup", this.onPointerUp, this);
-    this.on("pointerupoutside", this.onPointerUp, this);
-    this.on("pointermove", this.pointerMoveHandler, this);
+    this.on('pointerdown', this.onPointerDown, this);
+    this.on('pointermove', this.onPointerMove, this);
+    this.on('pointerup', this.onPointerUp, this);
+    this.on('pointerupoutside', this.onPointerUp, this);
+    this.on('pointermove', this.pointerMoveHandler, this);
   }
 
   private tearDownEventListeners() {
-    this.off("pointerdown", this.onPointerDown, this);
-    this.off("pointermove", this.onPointerMove, this);
-    this.off("pointerup", this.onPointerUp, this);
-    this.off("pointerupoutside", this.onPointerUp, this);
-    this.off("pointermove", this.pointerMoveHandler, this);
+    this.off('pointerdown', this.onPointerDown, this);
+    this.off('pointermove', this.onPointerMove, this);
+    this.off('pointerup', this.onPointerUp, this);
+    this.off('pointerupoutside', this.onPointerUp, this);
+    this.off('pointermove', this.pointerMoveHandler, this);
   }
 
   private loadStore() {
@@ -96,7 +96,7 @@ export default class DecoDrawing extends Pixi.Container {
     this.addChild(graphicDraw);
 
     graphicDraw.interactive = true;
-    graphicDraw.on("pointerdown", (event: Pixi.FederatedPointerEvent) => {
+    graphicDraw.on('pointerdown', (event: Pixi.FederatedPointerEvent) => {
       const point = event.getLocalPosition(graphicDraw);
       this.drawPoint(point.x, point.y);
     });
@@ -117,37 +117,37 @@ export default class DecoDrawing extends Pixi.Container {
   private async setFaces() {
     const displacementData = [
       {
-        imagePath: "images/scene/map1.jpg",
+        imagePath: 'images/scene/map1.jpg',
       },
       {
-        imagePath: "images/scene/map2.jpg",
+        imagePath: 'images/scene/map2.jpg',
       },
       {
-        imagePath: "images/scene/map3.jpg",
+        imagePath: 'images/scene/map3.jpg',
       },
       {
-        imagePath: "images/scene/map4.jpg",
+        imagePath: 'images/scene/map4.jpg',
       },
     ];
 
     const faceData = [
       {
-        imagePath: "images/scene/face1.png",
+        imagePath: 'images/scene/face1.png',
         position: { x: -1000, y: this.faceHeight },
         charNumber: 0,
       },
       {
-        imagePath: "images/scene/face2.png",
+        imagePath: 'images/scene/face2.png',
         position: { x: -1000, y: this.faceHeight },
         charNumber: 1,
       },
       {
-        imagePath: "images/scene/face3.png",
+        imagePath: 'images/scene/face3.png',
         position: { x: -1000, y: this.faceHeight },
         charNumber: 2,
       },
       {
-        imagePath: "images/scene/face4.png",
+        imagePath: 'images/scene/face4.png',
         position: { x: -1000, y: this.faceHeight },
         charNumber: 3,
       },
@@ -179,7 +179,7 @@ export default class DecoDrawing extends Pixi.Container {
 
       this.filter = new OutlineFilter(2, 0x000000);
 
-      const maskLoad = await Pixi.Assets.load("images/drawing/mini.png");
+      const maskLoad = await Pixi.Assets.load('images/drawing/mini.png');
       const sprite = Pixi.Sprite.from(maskLoad);
       sprite.width = 650;
       sprite.height = 700;
@@ -200,7 +200,7 @@ export default class DecoDrawing extends Pixi.Container {
       container.addChild(sprite);
       container.addChild(graphic);
 
-      const hairMaskLoad = await Pixi.Assets.load("images/drawing/mini2.png");
+      const hairMaskLoad = await Pixi.Assets.load('images/drawing/mini2.png');
       const hairSprite = Pixi.Sprite.from(hairMaskLoad);
       hairSprite.width = 700;
       hairSprite.height = 200;
@@ -217,6 +217,10 @@ export default class DecoDrawing extends Pixi.Container {
       hairGraphic.drawRect(0, 0, Setting.sceneWidth, Setting.sceneHeight);
       hairGraphic.pivot.set(center.x, center.y);
       hairGraphic.position.set(center.x, center.y);
+
+      const coordinates = Data.hairCoordinates[0].coordinates;
+      const points = coordinates.flatMap(({ x, y }) => [x, y]);
+      hairGraphic.hitArea = new Pixi.Polygon(points);
       hairGraphic.endFill();
       if (this.displacementFilter) {
         hairGraphic.filters = [
@@ -234,6 +238,7 @@ export default class DecoDrawing extends Pixi.Container {
         hairSprite,
         hairGraphic,
         charNumber,
+        hairCoordinate: coordinates,
       };
       this.faces.push(face);
 
@@ -250,137 +255,25 @@ export default class DecoDrawing extends Pixi.Container {
   protected onPointerDown(e: Pixi.FederatedPointerEvent) {
     console.log(`x: ${e.globalY}, y:${e.globalY}`);
 
-    const outlineHair = [
-      { x: 538, y: 169 },
-      { x: 546, y: 163 },
-      { x: 552, y: 157 },
-      { x: 560, y: 152 },
-      { x: 568, y: 149 },
-      { x: 573, y: 586 },
-      { x: 586, y: 595 },
-      { x: 607, y: 129 },
-      { x: 620, y: 124 },
-      { x: 635, y: 120 },
-      { x: 646, y: 118 },
-      { x: 664, y: 115 },
-      { x: 674, y: 115 },
-      { x: 688, y: 117 },
-      { x: 702, y: 119 },
-      { x: 712, y: 123 },
-      { x: 724, y: 128 },
-      { x: 736, y: 133 },
-      { x: 754, y: 145 },
-      { x: 761, y: 155 },
-      { x: 774, y: 168 },
-      { x: 782, y: 177 },
-      { x: 790, y: 191 },
-      { x: 794, y: 198 },
-      { x: 798, y: 204 },
-      { x: 808, y: 213 },
-      { x: 823, y: 222 },
-      { x: 834, y: 229 },
-      { x: 845, y: 233 },
-      { x: 868, y: 233 },
-      { x: 894, y: 236 },
-      { x: 913, y: 237 },
-      { x: 929, y: 238 },
-      { x: 947, y: 238 },
-      { x: 962, y: 238 },
-      { x: 971, y: 238 },
-      { x: 974, y: 237 },
-      { x: 970, y: 226 },
-      { x: 971, y: 210 },
-      { x: 968, y: 202 },
-      { x: 962, y: 185 },
-      { x: 959, y: 170 },
-      { x: 948, y: 145 },
-      { x: 932, y: 123 },
-      { x: 910, y: 108 },
-      { x: 891, y: 97 },
-      { x: 869, y: 87 },
-      { x: 856, y: 81 },
-      { x: 836, y: 72 },
-      { x: 814, y: 68 },
-      { x: 793, y: 65 },
-      { x: 764, y: 61 },
-      { x: 720, y: 58 },
-      { x: 688, y: 59 },
-      { x: 658, y: 56 },
-      { x: 632, y: 54 },
-      { x: 589, y: 55 },
-      { x: 566, y: 57 },
-      { x: 514, y: 63 },
-      { x: 492, y: 65 },
-      { x: 458, y: 71 },
-      { x: 434, y: 79 },
-      { x: 423, y: 83 },
-      { x: 414, y: 89 },
-      { x: 402, y: 95 },
-      { x: 396, y: 105 },
-      { x: 387, y: 115 },
-      { x: 382, y: 120 },
-      { x: 379, y: 133 },
-      { x: 376, y: 141 },
-      { x: 372, y: 150 },
-      { x: 364, y: 160 },
-      { x: 358, y: 198 },
-      { x: 350, y: 184 },
-      { x: 338, y: 198 },
-      { x: 327, y: 214 },
-      { x: 324, y: 223 },
-      { x: 324, y: 229 },
-      { x: 338, y: 233 },
-      { x: 359, y: 237 },
-      { x: 384, y: 240 },
-      { x: 418, y: 238 },
-      { x: 441, y: 233 },
-      { x: 472, y: 220 },
-      { x: 491, y: 210 },
-      { x: 511, y: 193 },
-      { x: 524, y: 179 },
-      { x: 538, y: 169 },
-    ];
-    const outlineFace = [{ x: 20, y: 20 }];
-
-    type Coordinate = { x: number; y: number };
-    const isPointInPolygon = (
-      point: Coordinate,
-      polygon: Coordinate[]
-    ): boolean => {
-      let inside = false;
-      for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-        let xi = polygon[i].x,
-          yi = polygon[i].y;
-        let xj = polygon[j].x,
-          yj = polygon[j].y;
-
-        let intersect =
-          yi > point.y !== yj > point.y &&
-          point.x < ((xj - xi) * (point.y - yi)) / (yj - yi) + xi;
-        if (intersect) inside = !inside;
-      }
-      return inside;
-    };
-
-    let point = { x: e.pageX - this.x, y: e.pageY - this.y };
-
-    if (isPointInPolygon(point, outlineHair)) {
-      console.log("Clicked inside the hair outline!");
-      this.targetSelect("hair");
-    } else {
-      console.log("매우 개빡친다");
-      this.targetSelect("face");
-    }
-
     this.down = true;
-    this.prevX = e.pageX - this.x;
-    this.prevY = e.pageY - this.y;
+    this.prevX = e.globalX - this.x;
+    this.prevY = e.globalY - this.y;
+
+    const hitArea = this.faces[this.charNumber].hairGraphic.hitArea;
+
+    if (hitArea) {
+      if (hitArea.contains(this.prevX, this.prevY)) {
+        this.drawTarget = 'hair'
+      } else {
+        this.drawTarget = 'face'
+      }
+    }
   }
 
   protected onPointerMove(e: Pixi.FederatedPointerEvent) {
     if (this.down) {
       const board =
-        this.drawTarget === "hair"
+        this.drawTarget === 'hair'
           ? this.faces[this.charNumber].hairGraphic
           : this.faces[this.charNumber].graphic;
       if (board) {
@@ -407,7 +300,7 @@ export default class DecoDrawing extends Pixi.Container {
   private async setFacesPosition(direction: string) {
     this.faceMoveEnable(true);
 
-    if (direction === "right") {
+    if (direction === 'right') {
       const previousCharNumber = this.charNumber;
       this.charNumber = this.charNumber - 1;
       if (this.charNumber < 0) {
@@ -434,7 +327,7 @@ export default class DecoDrawing extends Pixi.Container {
       }
       await wait(1000);
       this.faceMoveEnable(false);
-    } else if (direction === "left") {
+    } else if (direction === 'left') {
       const previousCharNumber = this.charNumber;
       this.charNumber = this.charNumber + 1;
       if (this.charNumber >= this.faceContainer.length) {
@@ -462,7 +355,7 @@ export default class DecoDrawing extends Pixi.Container {
       }
       await wait(1000);
       this.faceMoveEnable(false);
-    } else if (direction === "default") {
+    } else if (direction === 'default') {
       this.faceContainer[this.charNumber].container.position.set(
         Setting.sceneWidth / 2,
         Setting.sceneHeight / 2
@@ -487,30 +380,30 @@ export default class DecoDrawing extends Pixi.Container {
   }
 
   private setButton() {
-    this.leftButtonSprite = Pixi.Sprite.from("images/scene/left.png");
+    this.leftButtonSprite = Pixi.Sprite.from('images/scene/left.png');
     this.leftButtonSprite.width = 70;
     this.leftButtonSprite.height = 70;
     this.leftButtonSprite.anchor.set(0.5);
     this.leftButtonSprite.position.set(300, Setting.sceneHeight / 2);
-    this.leftButtonSprite.eventMode = "static";
-    this.leftButtonSprite.cursor = "pointer";
-    this.leftButtonSprite.on("pointerdown", () => {
+    this.leftButtonSprite.eventMode = 'static';
+    this.leftButtonSprite.cursor = 'pointer';
+    this.leftButtonSprite.on('pointerdown', () => {
       if (!this.faceMoving) {
-        this.setFacesPosition("left");
+        this.setFacesPosition('left');
       }
     });
     this.addChild(this.leftButtonSprite);
 
-    this.rightButtonSprite = Pixi.Sprite.from("images/scene/right.png");
+    this.rightButtonSprite = Pixi.Sprite.from('images/scene/right.png');
     this.rightButtonSprite.width = 70;
     this.rightButtonSprite.height = 70;
     this.rightButtonSprite.anchor.set(0.5);
     this.rightButtonSprite.position.set(1000, Setting.sceneHeight / 2);
-    this.rightButtonSprite.eventMode = "static";
-    this.rightButtonSprite.cursor = "pointer";
-    this.rightButtonSprite.on("pointerdown", () => {
+    this.rightButtonSprite.eventMode = 'static';
+    this.rightButtonSprite.cursor = 'pointer';
+    this.rightButtonSprite.on('pointerdown', () => {
       if (!this.faceMoving) {
-        this.setFacesPosition("right");
+        this.setFacesPosition('right');
       }
     });
     this.addChild(this.rightButtonSprite);
